c3e3b1d9fce06dacad5f9bb056ed11ca0eeffdd3
DED8IRD <ewu72829@gmail.com>
2018-10-14 16:49:59 -0700

    initialize passport

--- server/index.js
+++ server/index.js
@@ -1,24 +1,28 @@
 // Express application
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const keys = require('./config/keys');
-const userAuthRoutes = require('./routes/userAuthRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const keys = require("./config/keys");
+
 const app = express();
-require('./models/User');
-require('./services/passport');
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+const userAuthRoutes = require("./routes/userAuthRoutes");
+require("./models/User");
+require("./services/passport");
 
 // Connect MongoDB instance
-mongoose.connect(keys.mongoURI)
+mongoose.connect(keys.mongoURI);
 mongoose.Promise = global.Promise;
 
 // Route handlers
 // Home
-app.get(
-	'/', 
-	(req, res) => {
-		res.send({ hello: 'world!' });
-	}
-);
+app.get("/", (req, res) => {
+	res.send({ hello: "world!" });
+});
 
 // Auth routes
 userAuthRoutes(app);
