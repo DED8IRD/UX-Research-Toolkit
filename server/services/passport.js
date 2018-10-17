20b69322c2ccc6e1479f1b27e7d56035631294d4
DED8IRD <ewu72829@gmail.com>
2018-10-17 15:07:27 -0700

    serialize/deserialize users

--- server/services/passport.js
+++ server/services/passport.js
@@ -11,8 +11,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-	.then(user => done(null, user));
+	User.findById(id).then(user => done(null, user));
 });
 
 passport.use(
@@ -22,24 +21,22 @@ passport.use(
 			clientSecret: keys.google.clientSecret,
 			callbackURL: "/auth/google/callback"
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleID: profile.id })
-			.then(existingUser => {
-				// If authorized user exists in database, query database for user's info
-				if (existingUser) {
-					console.log('Existing user')
-					console.log(existingUser)
-					done(null, existingUser);
-				
-				// Else create new user
-				} else {
-					console.log('Creating new user')
-					new User({
-						googleID: profile.id
-					}).save()
-					.then(newUser => done(null, newUser));
-				}
-			});
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleID: profile.id });
+			// If authorized user exists in database, query database for user's info
+			if (existingUser) {
+				console.log("Existing user");
+				console.log(existingUser);
+				done(null, existingUser);
+
+			// Else create new user
+			} else {
+				console.log("Creating new user");
+				const newUser = await new User({
+					googleID: profile.id
+				}).save();
+				done(null, newUser);
+			}
 		}
 	)
 );
